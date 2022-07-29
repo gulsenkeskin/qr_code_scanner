5b1689be43e8d9fd52d0c1f0313a559fc1048d31
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-30 01:43:46 +0300

    Update qr_code_scanner.dart

--- lib/screens/qr_code_scanner.dart
+++ lib/screens/qr_code_scanner.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qr_code_scanner/widgets/button_widget.dart';
 
 class QrCodeScanner extends StatefulWidget {
   const QrCodeScanner({Key? key}) : super(key: key);
@@ -8,10 +11,62 @@ class QrCodeScanner extends StatefulWidget {
 }
 
 class _QrCodeScannerState extends State<QrCodeScanner> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
+  String qrCode = '';
 
-    );
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Scan Result',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '$qrCode',
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 50),
+          ButtonWidget(
+            color: Colors.black26,
+            text: 'Start QR Scan',
+            onClicked: () => scanQRCode(),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.qrCode = qrCode.isEmpty
+            ? ''
+            : qrCode == '-1'
+            ? ''
+            : qrCode;
+      });
+    } on PlatformException {
+      qrCode = 'Failed to get platform version.';
+    }
   }
 }
