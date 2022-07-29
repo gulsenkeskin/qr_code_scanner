05b39545b384a531f577e67cafbdb48339087e7b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-30 01:28:23 +0300

    Create qr_code_scanner.dart

---
+++ lib/screens/qr_code_scanner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QrCodeScanner extends StatefulWidget {
+  const QrCodeScanner({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodeScanner> createState() => _QrCodeScannerState();
+}
+
+class _QrCodeScannerState extends State<QrCodeScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
