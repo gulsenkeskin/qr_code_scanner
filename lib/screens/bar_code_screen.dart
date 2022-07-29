8738e53e1c75e4576963084649244465772e73d2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-30 01:25:32 +0300

    added bottomNavigationBar

---
+++ lib/screens/bar_code_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class BarCodeScreen extends StatefulWidget {
+  const BarCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  _BarCodeScreenState createState() => _BarCodeScreenState();
+}
+
+class _BarCodeScreenState extends State<BarCodeScreen> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[Column(), Column()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Code Scanner  & QR Code Generator"),
+        backgroundColor: Colors.black54,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF313131),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code_scanner),
+              label: 'Scan Qr code',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code),
+              label: 'Create Qr Code',
+            ),
+          ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: const Color(0XFF5fa693),
+          iconSize: 40,
+          onTap: _onItemTapped,
+          elevation: 5),
+    );
+  }
+}
