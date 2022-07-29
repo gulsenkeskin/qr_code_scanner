6ad28afbab309ce8d36c19bb18fdc03cbf760114
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-30 01:28:21 +0300

    Update bar_code_screen.dart

---
+++ lib/screens/qr_code_generator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QrCodeGenerator extends StatefulWidget {
+  const QrCodeGenerator({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodeGenerator> createState() => _QrCodeGeneratorState();
+}
+
+class _QrCodeGeneratorState extends State<QrCodeGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
